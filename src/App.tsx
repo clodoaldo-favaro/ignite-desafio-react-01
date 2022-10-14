878ab1407391eae91b9a407192a1157348e39fc1
Clodoaldo Favaro <clodoaldo.favero@bling.com.br>
2022-10-13 22:18:54 -0300

    wip: cria input e botao de adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Header } from './components/Header';
 
 import './global.css';
+import styles from './App.module.css';
 
 
 export function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      
+      <form className={styles.taskForm}>
+        <input type="text" placeholder="Adicione uma nova tarefa"/>
+        <button type="submit">Criar</button>
+      </form>
+      
+    </div>
   );
 }
 
