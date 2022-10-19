a5fd656b658b6bc923dde0cb936b8db19aa9747a
Clodoaldo Favaro <clodoaldo.favero@bling.com.br>
2022-10-18 22:48:53 -0300

    feat: inclui div de tarefas vazias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { PlusCircle } from 'phosphor-react';
+import clipboard from './assets/clipboard.svg';
 
 import './global.css';
 import styles from './App.module.css';
@@ -29,6 +30,11 @@ export function App() {
               <span className={styles.tasksCompleted}>Concluídas</span>
               <div className={styles.tasksCounter}><span>0</span></div>
             </div>
+          </div>
+          <div className={styles.emptyTasks}>
+            <img src={clipboard} alt="" />
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
           </div>
         </div>
       </div>
